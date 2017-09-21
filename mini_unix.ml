82cd4dd0c1f45eaeb4e8278c23241b0a15f39a07
Tom Ridge <tom.j.ridge@googlemail.com>
2017-09-21 21:02:57 +0100

    +

--- mini_unix.ml
+++ mini_unix.ml
@@ -27,9 +27,12 @@ module T = Make(S)
 open S
 open T
 
+
 let err x = fun s -> (Error x,s)
 
-let with_state : (state -> state) -> unit m = failwith ""
+let safely : 'a m -> 'a m = fun x -> x  (* wrap in exception handling *)
+
+let with_state : (state -> 'a * state) -> 'a m = fun f -> failwith ""  (* use safely *)
 
 let bind : ('a -> 'b m) -> 'a m -> 'b m = fun f x s ->
   match x s with
@@ -37,6 +40,8 @@ let bind : ('a -> 'b m) -> 'a m -> 'b m = fun f x s ->
   | (Error _,_) as e -> e
 
 let return : 'a -> 'a m = fun x s -> (Ok x,s)
+
+
 
 exception No_such_entry
 
@@ -47,36 +52,28 @@ let mk_ops () =
   let root : path = "/" in
 
 
-  let unix_unlink ~parent ~name = Unix.unlink @@ parent^"/"^name in
-
-
-  (* FIXME or just allow unlink with no expectation of the kind? *)
-
-
   (* FIXME make these use "with state", which traps errors, and returns unit *)
   let unlink ~parent ~name = 
-    fun s ->
-      unix_unlink ~parent ~name;
-      (Ok (),s)
+    with_state @@ fun s ->
+    Unix.unlink @@ parent^"/"^name ;
+    ((),s)
   in
 
 
   let default_perm = 0o640 in
 
 
-  let unix_mkdir ~parent ~name = Unix.mkdir (parent^"/"^name) default_perm in
-
   let mkdir ~parent ~name : unit m = 
-    fun s -> 
-      unix_mkdir ~parent ~name;
-      (Ok (),s)
+    with_state @@ fun s -> 
+    Unix.mkdir (parent^"/"^name) default_perm;
+    ((),s)
   in
 
 
 
   let mk_dh ~path = Unix.opendir path in
 
-  let opendir path = mk_dh path |> return in
+  let opendir path = safely (mk_dh path |> return) in
 
 
   let readdir dh = 
@@ -85,56 +82,59 @@ let mk_ops () =
   in
 
 
-  let closedir dh = Unix.closedir dh; return () in  
+  let closedir dh = safely (Unix.closedir dh; return ()) in  
   (* FIXME should we record which dh are valid? ie not closed *)
 
 
   let create ~parent ~name : unit m = 
-    Unix.(fun s -> 
-        openfile (parent^"/"^name) [O_CREAT] default_perm |> fun fd ->
-        close fd;
-        (Ok (), s))
+    with_state @@ fun s -> 
+    let open Unix in
+    openfile (parent^"/"^name) [O_CREAT] default_perm |> fun fd ->
+    close fd;
+    ((), s)
   in
 
 
   let mk_fd path = Unix.(openfile path [O_RDWR] default_perm) in
 
-  let open_ path = mk_fd path |> return in
+  let open_ path = safely (mk_fd path |> return) in
 
 
   let pread ~fd ~foff ~length ~buffer ~boff = 
-    fun s ->
-      ExtUnix.All.pread fd foff buffer boff length |> fun nread ->
-      (Ok nread,s)
+    with_state @@ fun s ->
+    ExtUnix.All.pread fd foff buffer boff length |> fun nread ->
+    (nread,s)
   in
 
 
   let pwrite ~fd ~foff ~length ~buffer ~boff = 
-    fun s ->
-      ExtUnix.All.pwrite fd foff (Bytes.to_string buffer) boff length |> fun n ->
-      (Ok n,s)
+    with_state @@ fun s ->
+    ExtUnix.All.pwrite fd foff (Bytes.to_string buffer) boff length |> fun n ->
+    (n,s)
   in
 
 
-  let close fd = Unix.close fd; return () in (* FIXME record which are open? *)
+  let close fd = safely (Unix.close fd; return ()) in (* FIXME record which are open? *)
 
 
   let truncate ~path ~length = 
-    fun s ->
-      Unix.truncate path length; 
-      (Ok (),s)
+    with_state @@ fun s ->
+    Unix.truncate path length; 
+    ((),s)
   in
 
 
   let stat_file path = 
-    fun s -> Unix.(
-        stat path |> fun st ->
-        st.st_size |> fun sz ->        
-        (Ok{sz},s))
+    with_state @@ fun s -> 
+    let open Unix in
+    stat path |> fun st ->
+    st.st_size |> fun sz ->        
+    ({sz},s)
   in
 
 
   let kind path : st_kind m = 
+    safely @@ 
     Unix.(
       stat path |> fun st ->
       st.st_kind |> (function
